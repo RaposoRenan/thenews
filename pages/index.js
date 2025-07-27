a6af42f371f4bbb2161ab9b2e76abfcce8494e23
Renan Raposo <134230060+RaposoRenan@users.noreply.github.com>
2025-07-27 23:57:26 +0000

    atualização `index.js`

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,4 @@ function Home() {
   return <h1> Piriri, pororó.. Se você ama Renan Raposo, dê uma risadinha!</h1>;
 }
 
-function teste2() {
-  console.log("indentação errada");
-}
-
 export default Home;
